e154335d24e26ef673082c40bc528247f7ee28d9
J1min <aoiharu41@gmail.com>
2023-03-21 17:25:13 +0900

    Feat: 기본 폰트 noto sans kr 추가

--- styles/userStyle.ts
+++ styles/userStyle.ts
@@ -2,21 +2,14 @@ import { createGlobalStyle } from "styled-components";
 
 const UserStyle = createGlobalStyle` 
 @font-face {
-  font-family: 'Pretendard-Regular';
-  src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
-  font-weight: 400;
-  font-style: normal;
-}
-
-@font-face {
   font-family: 'CWDangamAsac-Bold';
   src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2108@1.1/CWDangamAsac-Bold.woff') format('woff');
   font-weight: normal;
   font-style: normal;
 }
 
-html {
-  font-family: 'Pretendard-Regular';
+html {  
+  font-family: "Noto Sans KR";
 }
 `;
 
