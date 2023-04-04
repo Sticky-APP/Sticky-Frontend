4605a6550fc0e59c2f24a79ae06cf4a7338c8c08
J1min <aoiharu41@gmail.com>
2023-04-04 14:05:42 +0900

    Feat: 구글 아이콘 추가

--- assets/PostIT/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as BluePostIt } from "./Blue.svg";
-export { default as YellowPostIt } from "./Yellow.svg";
-export { default as YellowWithClipPostIt } from "./YellowWithClip.svg";
---
+++ assets/index.ts
@@ -0,0 +1,10 @@
+export { default as BluePostIt } from "./PostIT/Blue.svg";
+export { default as YellowPostIt } from "./PostIT/Yellow.svg";
+export { default as YellowWithClipPostIt } from "./PostIT/YellowWithClip.svg";
+
+export { default as Landing1 } from "./Landing/1.svg";
+export { default as Landing2 } from "./Landing/2.svg";
+export { default as Landing3 } from "./Landing/3.svg";
+export { default as Laptop } from "./Landing/laptop.svg";
+
+export { default as GoogleIcon } from "./icons/GoogleIcon.svg";
