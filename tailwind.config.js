91bb6fbc31289a4c26a604c0d651cd4d60156f51
J1min <aoiharu41@gmail.com>
2023-04-18 14:08:47 +0900

    feat(config): tailwind config 기본 색상 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,12 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#662AF1",
+        secondary: "#EBE4FB",
+      },
+    },
   },
   plugins: [],
 };
