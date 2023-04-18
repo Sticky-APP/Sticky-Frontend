f23d5da45804e897acd58eb73c54d382556a922e
jeongdonggyeom <topcome928@gmail.com>
2023-04-18 14:53:42 +0900

    Feat: 가구 아이콘 드래그 추가

--- components/space/Item.tsx
+++ components/space/Item.tsx
@@ -1,11 +1,16 @@
+import useDraggable from "@/hooks/useDraggable";
 import Image, { StaticImageData } from "next/image";
 import styled from "styled-components";
 
 export default function Item({ item }: { item: StaticImageData }) {
+  const { CustomDraggableView } = useDraggable();
+
   return (
-    <ItemContainer>
-      <Image src={item} width={100} height={90} alt="icon" />
-    </ItemContainer>
+    <CustomDraggableView>
+      <ItemContainer>
+        <Image src={item} width={100} height={90} alt="icon" />
+      </ItemContainer>
+    </CustomDraggableView>
   );
 }
 
